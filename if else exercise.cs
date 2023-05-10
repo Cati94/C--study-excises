46203d1bb637dc4d0c46a0d5bfb5bd1ef2ff2144
Catarina Correia <73751126+Cati94@users.noreply.github.com>
2023-05-10 20:28:32 +0100

    Add files via upload
    Exercicio if e if else continuação do programa básico

---
+++ if else exercise.cs
@@ -0,0 +1,23 @@
+﻿// condicionales if e else if //
+using System;
+namespace cursobasico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int scholl = 300;
+            int medical = 200;
+            int bus = 150;
+
+            if (scholl < medical)
+            {
+                Console.WriteLine(scholl + "its lower than" + medical);
+            }
+            else
+            {
+                Console.WriteLine(scholl + "its higther than" + bus);
+            }
+        }
+    }
+}
